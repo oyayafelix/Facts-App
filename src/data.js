12e68738a933c64e19a664685aa213e05add9a36
oyayafelix <oyayafelix@gmail.com>
2022-08-04 11:19:00 +0300

    updated file structure

--- src/data.js
+++ src/data.js
@@ -3,28 +3,27 @@ const DATA = [
         id: 1,
         category:"Kenyan",
         country: 'Kenya',
-        faq:'Kenya is a tourist destination'
-        
+        faq:'Kenya is a country in East Africa with coastline on the Indian Ocean. It encompasses savannah, lakelands, the dramatic Great Rift Valley and mountain highlands. It also home to wildlife like lions, elephants and rhinos. From Nairobi, the capital, safaris visit the Maasai Mara Reserve, known for its annual wildebeest migrations, and Amboseli National Park, offering views of Tanzania 5,895m Mt. Kilimanjaro.'
     },
     {   
         id: 2,
         category:"Jamaican",
         country: 'Jamaica',
-        faq:'Jamaica is a tourist destination'
+        faq:'Jamaica, a Caribbean island nation, has a lush topography of mountains, rainforests and reef-lined beaches. Many of its all-inclusive resorts are clustered in Montego Bay, with its British-colonial architecture, and Negril, known for its diving and snorkeling sites. Jamaica is famed as the birthplace of reggae music, and its capital Kingston is home to the Bob Marley Museum, dedicated to the famous singer.'
         
     },
     {   
         id: 3,
         category:"Tanzanian",
         country: 'Tanzania',
-        faq:'Kenya is a tourist destination'
+        faq:'Tanzania is an East African country known for its vast wilderness areas. They include the plains of Serengeti National Park, a safari mecca populated by the “big five” game (elephant, lion, leopard, buffalo, rhino), and Kilimanjaro National Park, home to Africa’s highest mountain. Offshore lie the tropical islands of Zanzibar, with Arabic influences, and Mafia, with a marine park home to whale sharks and coral reefs.'
         
     },
     {   
         id: 4,
         category:"Mexican",
         country: 'Mexico',
-        faq:'Mexico is a tourist destination'
+        faq:'Mexico, officially the United Mexican States, is a country in the southern portion of North America. It is bordered to the north by the United States; to the south and west by the Pacific Ocean; to the southeast by Guatemala, Belize, and the Caribbean Sea; and to the east by the Gulf of Mexico. '
         
     }
 ]
