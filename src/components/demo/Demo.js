60e5742b514d1be23794745e0bbeabb0de624746
oyayafelix <oyayafelix@gmail.com>
2022-08-04 19:49:46 +0300

    rename data.js to data.json

--- src/components/demo/Demo.js
+++ src/components/demo/Demo.js
@@ -9,8 +9,8 @@ import {apiUrl } from '../api';
 function Demo(){
 
     const [data, setData] = useState(DATA);
-    const [countries, setCounries] =useState([]);
-    const [countries, setCounries] =useState("");
+    const [countries, setCountries] =useState([]);
+    const [error, setError] =useState("");
 
     const getAllCountries = async () => {
         try {
@@ -55,7 +55,34 @@ function Demo(){
                 )
             })}  
         </div>
+        <div className="countries">
+  {error && <h3>{error}</h3>}
+  {countries?.map((country) => (
+    <div className="allCountries" key={country.name.common}>
+              <img className="flag" src={country.flags.png} alt="flag" />
 
+              <div className="all-countries-detail mainDark">
+                <h2 className="all-countries-name">{country.name.common}</h2>
+                <p className="all-countries-population">
+Population:
+                  <span>
+                    {new Intl.NumberFormat().format(country.population)}
+                  </span>
+                </p>
+                <p className="all-countries-region">
+                  Region: <span>{country.region}</span>
+                </p>
+                <p className="all-countries-capital">
+                  Capital: <span>{country.capital}</span>
+                </p>
+              </div>
+    </div>
+  ))}
+</div>
+
+
+
+            
     </div>
     );
 }
