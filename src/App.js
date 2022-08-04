31c5e33e5964612c9b719ab720fee690274a8763
oyayafelix <oyayafelix@gmail.com>
2022-08-04 20:31:53 +0300

    add landing-page to its component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import './App.css';
-import image1 from './img/image1.jpg';
 import Demo from './components/demo/Demo';
-// import DATA from './data';
-// import {nanoid} from 'nanoid';
-// import { useState } from 'react';
-
-
 
 function App() {
-  // const [facts, setFacts] = useState(DATA);
 
-  // const getFactByCountry = async(country) =>{
-  //   const res = await fetch(`${DATA}`)
-
-  //   // console.log(res)
-  // }
-  // getFactByCountry()
 
   return (
     <div className="App">
@@ -41,19 +28,7 @@ function App() {
           </ul>
         </div>
       </nav>
-      <div className='landing-page'>
-        <div className='landing-page-img'>
-          <img src={image1} alt='' />
-        </div>
-        <div className='about-us'>
-          <h1>About Us</h1>
-          <h2>Investment Facts</h2>
-          <p>
-          Get facts and information from credible sources to assist you with decision making and investing. Source data from over 1000 sources in different industries. This generated data is converted to meaningful information to asist investors in decision making from an informed background.
-          </p>
-          <button>Learn more</button>
-        </div>
-      </div>
+      
       <Demo
       />
     </div>
---
+++ src/components/landing-page/Landing-page.js
@@ -0,0 +1,23 @@
+import image1 from './img/image1.jpg';
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <div className="landing-page-img">
+        <img src={image1} alt="" />
+      </div>
+      <div className="about-us">
+        <h1>About Us</h1>
+        <h2>Investment Facts</h2>
+        <p>
+          Get facts and information from credible sources to assist you with
+          decision making and investing. Source data from over 1000 sources in
+          different industries. This generated data is converted to meaningful
+          information to asist investors in decision making from an informed
+          background.
+        </p>
+        <button>Learn more</button>
+      </div>
+    </div>
+  );
+}
