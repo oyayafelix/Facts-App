ee534ae8c09878b68baf2c0b17c91ab284cd3008
oyayafelix <oyayafelix@gmail.com>
2022-08-04 23:35:47 +0300

    added about component

--- src/components/Industries/Industries.js
+++ src/components/Industries/Industries.js
@@ -1,8 +1,57 @@
 import './Industries.css';
+import api from '../api';
+import {useState, useEffect} from 'react';
 
 function Industries(){
+    const [industries, setIndustries] = useState([]);
+    const [formData, setFormData] = useState({industry: ""});
+
+    //get all industries
+    useEffect(() => {
+        fetch("http://localhost:3001/industries")
+        .then((response) => response.json())
+        .then((data)=>{
+            setIndustries(data)
+            // console.log(data[0].name)
+        })
+    },[]);
+    
+    const renderIndustries = industries.map((industry) => {
+        return(
+            <div  key={industry.id}>
+                <ul>
+                    <li>
+                    {industry.name} --
+                    {industry.rating}
+                    </li>
+                </ul>
+                
+            </div>
+        )
+    })
+    function handleChange(event){
+        setFormData(event.target.value,);
+        console.log(event.target.value)
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        addIndustry()
+        
+    }
+
+    function addIndustry(){
+        console.log('Submitted')
+    }
     return(
-        <div>Industires</div>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name='industry' value={formData.industry} onChange={handleChange}/>
+                <button>Submit</button>
+            </form>
+            {renderIndustries}
+
+        </div>
     )
 }
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -14,8 +14,8 @@ function Navbar(){
           <div className="menu">
             <ul>
               <li><Link to="/">Home</Link></li>
-              <li><Link to="/about">About</Link></li>
               <li><Link to="/industries">Industries</Link></li>
+              <li><Link to="/about">About</Link></li>
             </ul>
           </div>
         </nav>
